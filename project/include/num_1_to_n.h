29f04de6d1a277da5eed922627d6ed39640db8be
PetrPamuzhak <petromars444@gmail.com>
2021-10-18 17:42:12 +0300

    rename function numbers v.3

--- project/include/num_1_to_n.h
+++ project/include/num_1_to_n.h
@@ -1,6 +1,6 @@
-#ifndef PROJECT_INCLUDE_OUTPUT_NUM_1_TO_N_H_
-#define PROJECT_INCLUDE_OUTPUT_NUM_1_TO_N_H_
+#ifndef PROJECT_INCLUDE_NUM_1_TO_N_H_
+#define PROJECT_INCLUDE_NUM_1_TO_N_H_
 
 int num_1_to_n(int n, int m);
 
-#endif   // PROJECT_INCLUDE_OUTPUT_NUM_1_TO_N_H_
+#endif   // PROJECT_INCLUDE_NUM_1_TO_N_H_
