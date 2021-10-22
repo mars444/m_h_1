6a1b2cfaf09eacdf7e57a81ea6277c71cfd23f21
PetrPamuzhak <petromars444@gmail.com>
2021-10-23 02:06:00 +0300

    working casses? but error Files mismatches

--- project/include/main.h
+++ project/include/main.h
@@ -1,4 +1,28 @@
-#define  PROJECT_INCLUDE_MAIN_H_
 #ifndef  PROJECT_INCLUDE_MAIN_H_
+#include <stdio.h>
+#define  PROJECT_INCLUDE_MAIN_H_
+
+
+
+struct masterRecord { int Number;
+                      char Name[20];
+                      char Surname[20];
+                      char addres[30];
+                      char TelNumber[15];
+                      double indebtedness;
+                      double credit_limit;
+                      double cash_payments;};
+typedef struct masterRecord Data;
+
+
+
+void masterWrite(FILE *ofPTR, Data Client);
+void transactionWrite(FILE *ofPTR2, Data transfer);
+void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
+
+    Data client_data;
+    Data transfer;
+
+
 #endif   //  PROJECT_INCLUDE_MAIN_H_
 //   pragma ""
--- project/src/main.c
+++ project/src/main.c
@@ -32,7 +32,7 @@ int main(void) {
 
     printf("%s", "please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
 
-    while ( scanf("%d", &choice) != 1 ) {
+    while ( scanf("%d", &choice) != -1 ) {
         switch (choice) {
         case 1:
             Ptr = fopen("record.dat", "r+");   // r+  Открывает файл для чтения/записи
@@ -93,7 +93,7 @@ void masterWrite(FILE *ofPTR, Data Client) {
                  Client.TelNumber,
                  &Client.indebtedness,
                  &Client.credit_limit,
-                 &Client.cash_payments) != 8 ) {
+                 &Client.cash_payments) != -1 ) {
         fprintf(ofPTR, "%-12d%-11s%-11s%-11s%-16s%20f%12.2f%12.2f\n",    // 12 11 выведет максимум
                 Client.Number,
                 Client.Name,
@@ -121,7 +121,7 @@ void transactionWrite(FILE *ofPTR2, Data transfer) {
     printf("%s\n%s\n",
            "1 Number account: ",
            "2 Client cash payments: ");
-    while ( scanf("%d %lf", &transfer.Number, &transfer.cash_payments) != 0 ) {
+    while ( scanf("%d %lf", &transfer.Number, &transfer.cash_payments) != -1 ) {
         fprintf(ofPTR2, "%-3d%-6.2f\n", transfer.Number, transfer.cash_payments);
         printf("%s  %d\n%s  %lf\n",
                "1 Number account:", transfer.Number,
@@ -139,8 +139,8 @@ void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data
                   client_data.TelNumber,
                   &client_data.indebtedness,
                   &client_data.credit_limit,
-                  &client_data.cash_payments) != 8 ) {
-        while ( fscanf(ofPTR_2, "%d %lf", &transfer.Number, &transfer.cash_payments) != 0 ) {
+                  &client_data.cash_payments) != -1 ) {
+        while ( fscanf(ofPTR_2, "%d %lf", &transfer.Number, &transfer.cash_payments) != -1 ) {
             if (client_data.Number == transfer.Number && transfer.cash_payments != 0) {
                 client_data.credit_limit += transfer.cash_payments;
             }
