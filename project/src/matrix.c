603286eefeba4e3cb3f8a2ea02ad2fde0d7a87d2
PetrPamuzhak <petromars444@gmail.com>
2021-10-29 23:43:00 +0300

    working functions create_matrix_from_file and create_matrix

--- project/src/matrix.c
+++ project/src/matrix.c
@@ -3,42 +3,44 @@
 #include <stdlib.h>
 
 Matrix* create_matrix(size_t rows, size_t cols) {
-    int** matrix;
-    size_t i, j, k;
-    matrix = (int**)malloc(rows*sizeof(int*));
+    Matrix* matrix;
+    matrix = (Matrix *)malloc(sizeof(Matrix));
+    size_t i, j;
+    matrix->arr = (double**)malloc(rows*sizeof(double*));
     for ( i = 0; i < rows; i++ ) {
-        matrix[i]=(int*)malloc(cols*sizeof(int));
+        matrix->arr[i]=(double*)malloc(cols*sizeof(double));
         for ( j = 0; j < cols; j++ ) {
-            matrix[i][j] = 0;
+            matrix->arr[i][j] = 0;
+            int a = matrix->arr[i][j];
+            printf("%5d ", a );
         }
+        printf("\n");
     }
     return matrix;
 }
 
 Matrix* create_matrix_from_file(const char* path_file) {
-    int** matrix;
-    int  i, j ,k;
+    Matrix* matrix;
+    matrix = (Matrix *)malloc(sizeof(Matrix));
+    int  i, j;
     int a;
-    FILE* qwe;
-    qwe = fopen(path_file, "r");
+    FILE* Ptr;
+    Ptr = fopen(path_file, "r");
     int rows, cols;
-        fscanf(qwe, "%d", &rows);
-        fscanf(qwe, "%d", &cols);
-        matrix = (int**)malloc(rows*sizeof(int*));
-        for (i = 0; i < rows; i++)
-            matrix[i] = (int*)malloc(cols*sizeof(int));
+        fscanf(Ptr, "%d", &rows);
+        fscanf(Ptr, "%d", &cols);
+         matrix->arr = (double**)malloc(rows*sizeof(double*));
         for (i = 0; i < rows; i++) {
+            matrix->arr[i] = (double*)malloc(cols*sizeof(double));
             for (j = 0; j < cols; j++) {
-                fscanf(qwe, "%d", &a);
+                fscanf(Ptr, "%d", &a);
                 printf("%5d",  a);
-                *(*(matrix+i)+j) = a;
+                matrix->arr[i][j] = a;
       }
       printf("\n");
     }
-    fclose(qwe);
-    for ( k = 0; k < rows; k++ ) {
-        free(matrix[k]);
-    }
-    free(matrix);
-    return 0;
+    printf("\n");
+    fclose(Ptr);
+    return matrix;
 }
+
