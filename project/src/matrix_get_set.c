1eb3c676fc65be580bf2b323ca940271915b4e23
PetrPamuzhak <petromars444@gmail.com>
2021-11-04 13:48:00 +0300

    add checks matrix_get_set

--- project/src/matrix_get_set.c
+++ project/src/matrix_get_set.c
@@ -3,21 +3,33 @@
 #include <stdlib.h>
 
 int get_rows(const Matrix *matrix, size_t *rows) {
+    if (matrix == NULL || rows == NULL) {
+        return -1;
+    }
     *rows = matrix->rows;
     return 0;
 }
 
 int get_cols(const Matrix *matrix, size_t *cols) {
+    if (matrix == NULL || cols == NULL) {
+        return -1;
+    }
     *cols = matrix->cols;
     return 0;
 }
 
 int get_elem(const Matrix *matrix, size_t row, size_t col, double *val) {
+    if (matrix == NULL) {
+        return -1;
+    }
     *val = matrix->arr[row][col];
     return 0;
 }
 
 int set_elem(Matrix *matrix, size_t row, size_t col, double val) {
+    if (matrix == NULL) {
+        return -1;
+    }
     matrix->arr[row][col] = val;
     return 0;
 }
