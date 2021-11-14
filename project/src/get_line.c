e5f0706c303db82a7e5d508ec0a654eb5109d813
PetrPamuzhak <petromars444@gmail.com>
2021-11-15 01:16:14 +0300

    get 1 line from file

--- project/src/get_line.c
+++ project/src/get_line.c
@@ -1,5 +1,24 @@
 #include "../include/get_line.h"
+#include <string.h>
 
-void get_line(char *path_to_file) {
+int get_line(const char *path_to_file) {
 
+    int file_eml = open(path_to_file, O_RDONLY);
+
+    if (file_eml == 0) {
+        return -1;
+    }
+
+    struct stat buf;
+    char *one_line;
+
+    fstat(file_eml, &buf);
+
+    one_line = (char *)mmap(0, buf.st_size, PROT_READ, MAP_SHARED, file_eml, 0);
+    printf("%s\n", one_line);
+
+    char *a = strstr(one_line, "</HTML>");
+    printf("%s\n", a);
+
+    return 0;
 }
--- project/src/main.c
+++ project/src/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "get_line.c"
 
 int main() {
 
   const char *path_to_eml = "1.eml";
 
-  get_line(path_to_eml);
+    get_line(path_to_eml);
 
   
   
 
   return 0;
 }
-//  start
-//wrgvsrbhdfnh
