4eb46eb608864e24c61b624f321e4026c51d7400
PetrPamuzhak <petromars444@gmail.com>
2021-11-20 14:24:07 +0300

    fix get_line func

--- project/src/main.c
+++ project/src/main.c
@@ -7,35 +7,35 @@
 
 int main(int argc, char **argv){
 
-  if (argc != 2) {
-    return -1;
-  }
+    if (argc != 2) {
+        return -1;
+    }
 
-  char *path_to_eml = argv[1];
+    char *path_to_eml = argv[1];
 
-  char *one_line = get_line(path_to_eml);
+    char *one_line = get_line(path_to_eml);
 
-  eml_t *from = parser(one_line, FROM);
-  printf("%s|", from->mail_target);
-  free_eml_t(from);
+    eml_t *from = parser(one_line, FROM);
+    printf("%s|", from->mail_target);
+    free_eml_t(from);
 
-  eml_t *to = parser(one_line, TO);
-  printf("%s|", to->mail_target);
-  free_eml_t(to);
+    eml_t *to = parser(one_line, TO);
+    printf("%s|", to->mail_target);
+    free_eml_t(to);
 
-  eml_t *date = parser(one_line, DATE);
-  printf("%s|", date->mail_target);
-  free_eml_t(date);
+    eml_t *date = parser(one_line, DATE);
+    printf("%s|", date->mail_target);
+    free_eml_t(date);
 
-  eml_t *content = parser(one_line, CONTENT_TYPE);
+    eml_t *content = parser(one_line, CONTENT_TYPE);
 
-  char *boundary = get_boundary_value(content->mail_target);
+    char *boundary = get_boundary_value(content->mail_target);
 
-  int parts = parts_search(one_line, boundary);
+    int parts = parts_search(one_line, boundary);
 
-  printf("%d\n", parts);
+    printf("%d\n", parts);
 
-  free_eml_t(content);
+    free_eml_t(content);
 
-  return 0;
+    return 0;
 }
